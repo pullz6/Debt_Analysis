8e1b7e47c766269ddae55d4ab073af5e58880f65
Sara <gunawardhanapulsara@gmail.com>
2024-06-22 23:01:34 +0100

    inital plot included

--- Web_App/analysis.py
+++ Web_App/analysis.py
@@ -1,11 +1,44 @@
 from dotenv import load_dotenv
 from pprint import pprint 
+import datetime
+import wbdata
+import numpy as np
+import pandas as pd
 import requests 
 import os 
 
+
+def formatNum(x):
+    """Creating a function that will change units to billions and round to 0 decimal places"""
+    y = x/1000000000
+    z = round(y)
+    return(z)
+
 def get_analysis(country='AGO'): 
-    debt_data = "Works "+country
-    return debt_data
+    countries = []
+    countries.append(country)
+    print(countries)
+    indicators = {"DT.DOD.DLXF.CD": "External_Debts","DT.DOD.DIMF.CD":"IMF_Credit","DT.DOD.DSTC.CD":"Short_term_debt","BM.GSR.TOTL.CD": "Income","FI.RES.TOTL.CD":"Total_Reserves","NY.GNP.MKTP.CD":"Gross_income","BX.GSR.TOTL.CD":"Exports","NE.IMP.GNFS.CD":"Imports","NE.DAB.TOTL.CN":"National_Expenditure","BN.GSR.FCTY.CD":"Primary_Income","BN.KLT.DINV.CD":"Foreign_Investment","BX.GRT.EXTA.CD.WD":"Grants","BX.GSR.GNFS.CD":"Exports","DT.NFL.SDGF.CD":"SDGFUND","DT.NFL.SPRP.CD":"SPRP","DT.NFL.UNAI.CD":"UNAIDS","DT.NFL.UNCD.CD":"UNCDF","DT.NFL.UNCF.CD":"UNICEF","DT.NFL.UNCR.CD":"UNHCR","DT.NFL.UNCTAD.CD":"UNCTAD","DT.NFL.UNCV.CD":"UNCOVID","DT.NFL.UNDP.CD":"UNDP","DT.NFL.UNEC.CD":"UNECE","DT.NFL.UNEP.CD":"UNEP","DT.NFL.UNFP.CD":"UNFPA","DT.NFL.UNID.CD":"UNIDIR","DT.NFL.UNIDO.CD":"UNIDO","DT.NFL.UNPB.CD":"UNPBF","DT.NFL.UNRW.CD":"UNRWA","DT.NFL.UNTA.CD":"UNTA","DT.NFL.UNWN.CD":"UNWOMEN","DT.NFL.UNWT.CD":"UNWTO","DT.NFL.WFPG.CD":"WFP","DT.NFL.WHOL.CD":"WHO","DT.NFL.WITC.CD":"WTO-ITC"}
+    df = wbdata.get_dataframe(indicators, country=countries, parse_dates=True, keep_levels =True)
+    df = pd.DataFrame(df.to_records())
+
+    df = df.replace(np.nan, 0)
+    df['Income'] = df['Income'].apply(formatNum)
+    df['External_Debts'] = df['External_Debts'].apply(formatNum)
+    df['Total_Reserves'] = df['Total_Reserves'].apply(formatNum)
+    df['IMF_Credit'] = df['IMF_Credit'].apply(formatNum)
+    df['Short_term_debt'] = df['Short_term_debt'].apply(formatNum)
+    df['Gross_income'] = df['Gross_income'].apply(formatNum)
+    df['Exports'] = df['Exports'].apply(formatNum)
+    df['Imports'] = df['Imports'].apply(formatNum)
+    df['National_Expenditure'] = df['National_Expenditure'].apply(formatNum)
+    df['Primary_Income'] = df['Primary_Income'].apply(formatNum)
+    df['Foreign_Investment'] = df['Foreign_Investment'].apply(formatNum)
+    df['Grants'] = df['Grants'].apply(formatNum)
+    print(df.head())
+    
+    # Use render_template to pass graphJSON to html
+    return df
 
 if __name__ == "__main__": 
     print('\n** Get the City for Debt Analysis**\n')
--- Web_App/server.py
+++ Web_App/server.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request 
 from analysis import get_analysis
 from waitress import serve
+import json
+import plotly
+import plotly.express as px
+import plotly.io as pio
+import plotly.graph_objects as go
 
 app = Flask(__name__)
 
@@ -13,11 +18,22 @@ def index():
 
 def get_country(): 
     country = request.args.get('country')
-    debt_data = get_analysis(country)
+    df = get_analysis(country)
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=df['date'], y=df['External_Debts'],mode='lines+markers', name='External Debts'))
+    fig.add_trace(go.Scatter(x=df['date'], y=df['IMF_Credit'], mode='lines+markers', name='IMF Credit'))
+    fig.add_trace(go.Scatter(x=df['date'], y=df['Short_term_debt'], mode='lines+markers', name='Short Term Debt'))
+    fig.add_trace(go.Scatter(x=df['date'], y=df['Gross_income'], mode='lines+markers', name='Income'))
+    fig.add_trace(go.Scatter(x=df['date'], y=df['Exports'], mode='lines+markers', name='Exports'))
+    fig.add_trace(go.Scatter(x=df['date'], y=df['Imports'], mode='lines+markers', name='Imports'))
+    fig.add_trace(go.Scatter(x=df['date'], y=df['National_Expenditure'], mode='lines+markers', name='National_Expenditure'))
+    # Create graphJSON
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    check = 'Works'
     return render_template(
         'analysis.html', 
         title=country,
-        temp = debt_data
+        graphJSON=graphJSON
     )
 
 if __name__ == "__main__": 
