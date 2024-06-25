a3c7fdcbe1ad150d890f9f44f9716bc3fa677215
Sara <gunawardhanapulsara@gmail.com>
2024-06-25 22:55:50 +0100

    Plots completed, transferred to web app

--- Web_App/analysis.py
+++ Web_App/analysis.py
@@ -18,7 +18,63 @@ def get_analysis(country='AGO'):
     countries = []
     countries.append(country)
     print(countries)
-    indicators = {"DT.DOD.DLXF.CD": "External_Debts","DT.DOD.DIMF.CD":"IMF_Credit","DT.DOD.DSTC.CD":"Short_term_debt","BM.GSR.TOTL.CD": "Income","FI.RES.TOTL.CD":"Total_Reserves","NY.GNP.MKTP.CD":"Gross_income","BX.GSR.TOTL.CD":"Exports","NE.IMP.GNFS.CD":"Imports","NE.DAB.TOTL.CN":"National_Expenditure","BN.GSR.FCTY.CD":"Primary_Income","BN.KLT.DINV.CD":"Foreign_Investment","BX.GRT.EXTA.CD.WD":"Grants","BX.GSR.GNFS.CD":"Exports","DT.NFL.SDGF.CD":"SDGFUND","DT.NFL.SPRP.CD":"SPRP","DT.NFL.UNAI.CD":"UNAIDS","DT.NFL.UNCD.CD":"UNCDF","DT.NFL.UNCF.CD":"UNICEF","DT.NFL.UNCR.CD":"UNHCR","DT.NFL.UNCTAD.CD":"UNCTAD","DT.NFL.UNCV.CD":"UNCOVID","DT.NFL.UNDP.CD":"UNDP","DT.NFL.UNEC.CD":"UNECE","DT.NFL.UNEP.CD":"UNEP","DT.NFL.UNFP.CD":"UNFPA","DT.NFL.UNID.CD":"UNIDIR","DT.NFL.UNIDO.CD":"UNIDO","DT.NFL.UNPB.CD":"UNPBF","DT.NFL.UNRW.CD":"UNRWA","DT.NFL.UNTA.CD":"UNTA","DT.NFL.UNWN.CD":"UNWOMEN","DT.NFL.UNWT.CD":"UNWTO","DT.NFL.WFPG.CD":"WFP","DT.NFL.WHOL.CD":"WHO","DT.NFL.WITC.CD":"WTO-ITC"}
+    indicators = {"DT.DOD.DLXF.CD": "External_Debts",
+              "DT.DOD.DIMF.CD":"IMF_Credit",
+              "DT.DOD.DSTC.CD":"Short_term_debt",
+              "BM.GSR.TOTL.CD": "Income",
+              "FI.RES.TOTL.CD":"Total_Reserves",
+              "NY.GNP.MKTP.CD":"Gross_income",
+              "BX.GSR.TOTL.CD":"Exports",
+              "NE.IMP.GNFS.CD":"Imports",
+              "NE.DAB.TOTL.CN":"National_Expenditure",
+              "BN.GSR.FCTY.CD":"Primary_Income",
+              "BN.KLT.DINV.CD":"Foreign_Investment",
+              "BX.GRT.EXTA.CD.WD":"Grants",
+              "BX.GSR.GNFS.CD":"Exports",
+              "DT.NFL.SDGF.CD":"SDGFUND",
+              "DT.NFL.SPRP.CD":"SPRP",
+              "DT.NFL.UNAI.CD":"UNAIDS",
+              "DT.NFL.UNCD.CD":"UNCDF",
+              "DT.NFL.UNCF.CD":"UNICEF",
+              "DT.NFL.UNCR.CD":"UNHCR",
+              "DT.NFL.UNCTAD.CD":"UNCTAD",
+              "DT.NFL.UNCV.CD":"UNCOVID",
+              "DT.NFL.UNDP.CD":"UNDP",
+              "DT.NFL.UNEC.CD":"UNECE",
+              "DT.NFL.UNEP.CD":"UNEP",
+              "DT.NFL.UNFP.CD":"UNFPA",
+              "DT.NFL.UNID.CD":"UNIDIR",
+              "DT.NFL.UNIDO.CD":"UNIDO",
+              "DT.NFL.UNPB.CD":"UNPBF",
+              "DT.NFL.UNRW.CD":"UNRWA",
+              "DT.NFL.UNTA.CD":"UNTA",
+              "DT.NFL.UNWN.CD":"UNWOMEN",
+              "DT.NFL.UNWT.CD":"UNWTO",
+              "DT.NFL.WFPG.CD":"WFP",
+              "DT.NFL.WHOL.CD":"WHO",
+              "DT.NFL.WITC.CD":"WTO-ITC",
+              "BM.GSR.MRCH.CD":"Goods_imports",
+              "BM.GSR.NFSV.CD":"Service_imports",
+              "BX.GSR.MRCH.CD":"Goods_exports",
+              "BX.GSR.NFSV.CD":"Service_exports",
+              "TM.VAL.AGRI.ZS.UN":"Agri_raw_materials_imports",
+              "TM.VAL.FOOD.ZS.UN":"Food_imports",
+              "TM.VAL.FUEL.ZS.UN":"Fuel_imports",
+              "TM.VAL.MANF.ZS.UN":"Manufacturing_imports",
+              "TM.VAL.MMTL.ZS.UN":"Ores_metal_imports",
+              "TX.VAL.AGRI.ZS.UN":"Agri_raw_materials_exports",
+              "TX.VAL.FOOD.ZS.UN":"Food_exports",
+              "TX.VAL.FUEL.ZS.UN":"Fuel_exports",
+              "TX.VAL.MANF.ZS.UN":"Manufacturing_exports",
+              "TX.VAL.MMTL.ZS.UN":"Ores_metal_exports",
+              "MS.MIL.XPND.CD":"Military_expenditure",
+              "NY.ADJ.AEDU.CD":"Education_expenditure",
+              "ST.INT.XPND.CD":"Tourism_expenditure",
+              "SH.XPD.CHEX.PC.CD":"Health_expenditure",
+              "SH.XPD.OOPC.PC.CD":"Out_of_pocket_expenditure",
+              "SH.XPD.PVTD.PC.CD":"Private_expenditure_health",
+              "NE.CON.PRVT.CD":"Household_expenditure"
+             }
     df = wbdata.get_dataframe(indicators, country=countries, parse_dates=True, keep_levels =True)
     df = pd.DataFrame(df.to_records())
 
@@ -35,6 +91,38 @@ def get_analysis(country='AGO'):
     df['Primary_Income'] = df['Primary_Income'].apply(formatNum)
     df['Foreign_Investment'] = df['Foreign_Investment'].apply(formatNum)
     df['Grants'] = df['Grants'].apply(formatNum)
+
+    #Funds 
+    df['SDGFUND'] = df['SDGFUND'].apply(formatNum)
+    df['SPRP'] = df['SPRP'].apply(formatNum)
+    df['UNAIDS'] = df['UNAIDS'].apply(formatNum)
+    df['UNCDF'] = df['UNCDF'].apply(formatNum)
+    df['UNICEF'] = df['UNICEF'].apply(formatNum)
+    df['UNHCR'] = df['UNHCR'].apply(formatNum)
+    df['UNCTAD'] = df['UNCTAD'].apply(formatNum)
+    df['UNCOVID'] = df['UNCOVID'].apply(formatNum)
+    df['UNDP'] = df['UNDP'].apply(formatNum)
+    df['UNECE'] = df['UNECE'].apply(formatNum)
+    df['UNEP'] = df['UNEP'].apply(formatNum)
+    df['UNFPA'] = df['UNFPA'].apply(formatNum)
+    df['UNIDIR'] = df['UNIDIR'].apply(formatNum)
+    df['UNRWA'] = df['UNRWA'].apply(formatNum)
+    df['UNTA'] = df['UNTA'].apply(formatNum)
+    df['UNWOMEN'] = df['UNWOMEN'].apply(formatNum)
+    df['UNWTO'] = df['UNWTO'].apply(formatNum)
+    df['WFP'] = df['WFP'].apply(formatNum)
+    df['WHO'] = df['WHO'].apply(formatNum)
+    df['WTO-ITC'] = df['WTO-ITC'].apply(formatNum)
+    df['Military_expenditure'] = df['Military_expenditure'].apply(formatNum)
+    df['Education_expenditure'] = df['Education_expenditure'].apply(formatNum)
+    df['Tourism_expenditure'] = df['Tourism_expenditure'].apply(formatNum)
+    df['Health_expenditure'] = df['Health_expenditure'].apply(formatNum)
+    df['Private_expenditure_health'] = df['Private_expenditure_health'].apply(formatNum)
+    df['Household_expenditure'] = df['Household_expenditure'].apply(formatNum)
+    df['Out_of_pocket_expenditure'] = df['Out_of_pocket_expenditure'].apply(formatNum)
+
+    df['UN_FUNDS'] = df['SDGFUND']+df['SPRP']+df['UNAIDS']+df['UNCDF']+df['UNICEF']+df['UNHCR']+df['UNCTAD']+df['UNCOVID']+df['UNDP']+df['UNECE']+df['UNEP']+df['UNFPA']+df['UNIDIR']+df['UNRWA']+df['UNTA']+df['UNWOMEN']+df['UNWTO']+df['WFP']+df['WHO']+df['WTO-ITC']
+    
     print(df.head())
     
     # Use render_template to pass graphJSON to html
--- Web_App/server.py
+++ Web_App/server.py
@@ -6,6 +6,7 @@ import plotly
 import plotly.express as px
 import plotly.io as pio
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 app = Flask(__name__)
 
@@ -19,6 +20,7 @@ def index():
 def get_country(): 
     country = request.args.get('country')
     df = get_analysis(country)
+
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=df['date'], y=df['External_Debts'],mode='lines+markers', name='External Debts'))
     fig.add_trace(go.Scatter(x=df['date'], y=df['IMF_Credit'], mode='lines+markers', name='IMF Credit'))
@@ -29,11 +31,59 @@ def get_country():
     fig.add_trace(go.Scatter(x=df['date'], y=df['National_Expenditure'], mode='lines+markers', name='National_Expenditure'))
     # Create graphJSON
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=df['date'], y=df['Goods_imports'],mode='lines+markers', name='Goods_imports'))
+    fig.add_trace(go.Scatter(x=df['date'], y=df['Service_imports'], mode='lines+markers', name='Service_imports'))
+    fig.add_trace(go.Scatter(x=df['date'], y=df['Goods_exports'], mode='lines+markers', name='Goods_exports'))
+    fig.add_trace(go.Scatter(x=df['date'], y=df['Service_exports'], mode='lines+markers', name='Service_exports'))
+
+    graphJSON2 = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+    df_2020_im = df[df['date'].dt.strftime('%Y') == '2022']
+    labels = ['Agriculteral Raw Material Imports','Food imports','Fuel imports','Manufacturing Imports','Ores Metals Imports']
+    values_im = [df_2020_im['Agri_raw_materials_imports'].iloc[0], df_2020_im['Food_imports'].iloc[0], df_2020_im['Fuel_imports'].iloc[0], df_2020_im['Manufacturing_imports'].iloc[0],df_2020_im['Ores_metal_imports'].iloc[0]]
+
+    df_2020_ex = df[df['date'].dt.strftime('%Y') == '2022']
+    values_ex = [df_2020_ex['Agri_raw_materials_exports'].iloc[0], df_2020_ex['Food_exports'].iloc[0], df_2020_ex['Fuel_exports'].iloc[0], df_2020_ex['Manufacturing_exports'].iloc[0],df_2020_ex['Ores_metal_exports'].iloc[0]]
+    fig = make_subplots(rows=1, cols=2, specs=[[{'type':'domain'}, {'type':'domain'}]])
+    fig.add_trace(go.Pie(labels=labels, values=values_im, name="Imports"),1, 1)
+    fig.add_trace(go.Pie(labels=labels, values=values_ex, name="Exports"), 1, 2)
+
+    # Use `hole` to create a donut-like pie chart
+    fig.update_traces(hole=.4, hoverinfo="label+percent+name")
+
+    fig.update_layout(
+        # Add annotations in the center of the donut pies.
+        annotations=[dict(text='Imports', x=0.16, y=0.5, font_size=20, showarrow=False),
+                    dict(text='Exports', x=0.84, y=0.5, font_size=20, showarrow=False)])
+    
+    graphJSON3 = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(x=df['date'], y=df['Military_expenditure'], fill='tonexty',
+                        mode='none',name='Military Expendiure' # override default markers+lines
+                        ))
+    fig.add_trace(go.Scatter(x=df['date'], y=df['Education_expenditure'], fill='tonexty',
+                        mode= 'none',name='Education Expendiure'))
+
+    fig.add_trace(go.Scatter(x=df['date'], y=df['Tourism_expenditure'], fill='tonexty',
+                        mode= 'none',name='Tourism Expendiure'))
+
+    fig.add_trace(go.Scatter(x=df['date'], y=df['Household_expenditure'], fill='tonexty',
+                        mode= 'none',name='Household Expendiure',fillcolor = 'lightblue'))
+    
+    graphJSON4 = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
     check = 'Works'
     return render_template(
         'analysis.html', 
         title=country,
-        graphJSON=graphJSON
+        graphJSON = graphJSON,
+        graphJSON2 = graphJSON2, 
+        graphJSON3 = graphJSON3,
+        graphJSON4 = graphJSON4
     )
 
 if __name__ == "__main__": 
